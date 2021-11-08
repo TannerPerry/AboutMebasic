362053c8b18bff3486dfa8584a52ee69cd59b219
Tanner Perry <tc-boy@hotmail.com>
2021-11-08 15:49:59 -0700

    Initial Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+let wizFirstName = "Harry"
+let wizLastname = "potter"
+let wizBirthYear = 1980
+let wizBirthPlace = "Great Britain"
+let wizBloodSatus = "half-blood"
+let wizGender = "male"
+var wizHeight = 71
+var wizWeight = 140
+
+//  part two
+
+let hasAppeared = true
+var appearedInPrequel = false
+let isDumbledoresArmy = false
+let isDeathEater = false
+
+//part three
+
+enum IsCrucial {
+    case crucial
+    case unimportant
+}
+
+let isImportant = IsCrucial.crucial
+
+let rating = 9
+
+let reasonForFav = "main charactor"
